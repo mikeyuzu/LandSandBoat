a3fc3167a1381b33599e8e97a4bb9144036827ca
mikeyuzu <agnet.tea@gmail.com>
2025-10-18 09:24:28 +0900

    裏闇王をLv75に調整。連続魔を廃止

--- scripts/zones/Dynamis-Xarcabard/mobs/Dynamis_Lord.lua
+++ scripts/zones/Dynamis-Xarcabard/mobs/Dynamis_Lord.lua
@@ -18,7 +18,7 @@ entity.onMobSpawn = function(mob)
             { id = xi.jsa.HUNDRED_FISTS,  hpp = 95 },
             { id = xi.jsa.MIGHTY_STRIKES, hpp = 95 },
             { id = xi.jsa.BLOOD_WEAPON,   hpp = 95 },
-            { id = xi.jsa.CHAINSPELL,     hpp = 95 },
+            --Lv75Т▓Ро { id = xi.jsa.CHAINSPELL,     hpp = 95 },
         },
     })
 end
