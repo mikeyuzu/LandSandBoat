cc3dd055dfcfa20534d677b0be1d4735ea155149
mikeyuzu <agnet.tea@gmail.com>
2025-12-02 22:31:44 +0900

    解明への灯クエストの受託条件変更

--- scripts/quests/crystalWar/Light_in_the_Darkness.lua
+++ scripts/quests/crystalWar/Light_in_the_Darkness.lua
@@ -23,7 +23,7 @@ quest.sections =
     {
         check = function(player, status, vars)
             return status == xi.questStatus.QUEST_AVAILABLE and
-                player:getCurrentMission(xi.mission.log_id.WOTG) == xi.mission.id.wotg.CAIT_SITH
+                player:getCurrentMission(xi.mission.log_id.WOTG) >= xi.mission.id.wotg.CAIT_SITH
         end,
 
         [xi.zone.BASTOK_MARKETS_S] =
